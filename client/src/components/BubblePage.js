2013203b272ddc6ba6117978580b254419f696e9
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-10-18 09:56:34 -0700

    bubbles rendering

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -10,7 +10,15 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
-
+ useEffect(() => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then(res => {
+        console.log(res)
+        setColorList(res.data)
+      })
+      .catch(err => console.log(err))
+    }, [])
   
 
   return (
